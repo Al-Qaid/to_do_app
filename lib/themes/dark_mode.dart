21874dd82cb23a4e4fafffa8a22a0632206f691e
Moh. Qaid <Mohammed Qaid>
2024-06-27 01:18:36 +0300

    #The first step is Create folder called themes inside it there are light and dark mode

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData darktMode = ThemeData(
+    colorScheme: ColorScheme.dark(
+  surface: Colors.grey.shade900,
+  primary: Colors.grey.shade600,
+  secondary: Colors.grey.shade700,
+  inversePrimary: Colors.grey.shade200,
+));
---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+    colorScheme: ColorScheme.light(
+  surface: Colors.grey.shade200,
+  primary: Colors.grey.shade600,
+  secondary: Colors.grey.shade700,
+  inversePrimary: Colors.grey.shade900,
+));
