3b52a42964eac1590a13e5ca753f8c2b4a9699e8
Moh. Qaid <Mohammed Qaid>
2024-06-28 00:00:30 +0300

    #Exporte utils folder from components folder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/components/utils/my_drawer.dart';
+import 'package:todo_app/utils/my_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
