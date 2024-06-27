39663fe6391ad7751cef2714100c05c9db6deba5
Moh. Qaid <Mohammed Qaid>
2024-06-28 00:15:26 +0300

    #Create todo_tile.dart file . #Add Slidable widget by run this command'flutter pub add flutter_slidable .

---
+++ lib/utils/todo_tile.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class TodoTile extends StatefulWidget {
+  final String taskName;
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteMethod;
+
+  const TodoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+    required this.deleteMethod,
+    });
+
+  @override
+  State<TodoTile> createState() => _TodoTileState();
+}
+
+class _TodoTileState extends State<TodoTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const StretchMotion(),
+          children: [
+            SlidableAction(
+              onPressed: deleteMethod,
+              icon: Icons.delete_forever,
+              backgroundColor: Theme.of(context).colorScheme.error,
+              borderRadius: BorderRadius.circular(40),
+            )
+          ],
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.onPrimaryFixedVariant,
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(width: 1.0),
+          ),
+          child: Row(
+            children: [
+              //checkbox
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Theme.of(context).colorScheme.inverseSurface,
+              ),
+
+              //Task name
+              Text(
+                taskName,
+                style: TextStyle(
+                    decoration: taskCompleted
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none,
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
